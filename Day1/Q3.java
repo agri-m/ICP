2291f95b2d5e8d2a1f12ba42688dacc7a0f2bab9
agri-m <agrim.k2005@gmail.com>
2025-09-26 23:54:18 +0530

    Create Q3.java

---
+++ Day1/Q3.java
@@ -0,0 +1,38 @@
+// Assignment Day 1 (Q2)
+//Leetcode 34. Find First and Last Position of Element in Sorted Array
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int start=0;
+        int end=nums.length-1;
+        int[] arr={-1,-1};
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                arr[0]=mid;
+                end=mid-1;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        start=0;
+        end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                arr[1]=mid;
+                start=mid+1;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+    return arr;
+    }
+}
